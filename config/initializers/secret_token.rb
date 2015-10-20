eaae881e4cc852eb98454d3928c1829e629d5c98
James Foster <jamesfoster85@gmail.com>
2015-10-20 13:20:55 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TombOfHorrors::Application.config.secret_key_base = '64df904e37ee645afaf6e36f054e17bb797495ede2c278b3a156e0503a7ba7996fc19732f52dcf10d1dd874695b61b9a3bb6818d836edbd4498b55d9a61e04b8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TombOfHorrors::Application.config.session_store :cookie_store, key: '_tomb_of_horrors_session'
